8e02f84305ad7b3b9d894827d87d115b84bd3372
Jorge Onrubia <jorge.onrubia.mojica@gmail.com>
2021-11-15 14:45:44 +0100

    First refactor

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -4,55 +4,73 @@ class GildedRose(var items: Array<Item>) {
 
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+            if (!isAgedBrie(items[i].name) && !isBackstagePass(items[i].name)) {
+                if (isQualityOverZero(items[i].quality) && !isSulfuras(items[i].name)) {
+                    decrementQualityBy(items[i], 1)
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (isLessThanMaximumQuality(items[i])) {
+                    incrementQualityBy(items[i], 1)
+                    if (isBackstagePass(items[i].name) && isLessThanMaximumQuality(items[i])) {
                         if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                            incrementQualityBy(items[i], 1)
                         }
-
                         if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                            incrementQualityBy(items[i], 1)
                         }
                     }
+
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+
+            if (!isSulfuras(items[i].name)) {
                 items[i].sellIn = items[i].sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (isExpired(items[i])) {
+                if (!isAgedBrie(items[i].name)) {
+                    if (!isBackstagePass(items[i].name)) {
+                        if (isQualityOverZero(i)) {
+                            if (isSulfuras(items[i].name)) {
+                                decrementQualityBy(items[i], 1)
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        setQualityToZero(items[i])
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (isLessThanMaximumQuality(items[i])) {
+                        incrementQualityBy(items[i], 1)
                     }
                 }
             }
         }
     }
+
+    private fun setQualityToZero(item: Item) {
+        item.quality = item.quality - item.quality
+    }
+
+    private fun isExpired(item: Item) = item.sellIn < 0
+
+    private fun incrementQualityBy(item: Item, i: Int) {
+        item.quality += i
+    }
+
+    private fun isLessThanMaximumQuality(item: Item) = item.quality < 50
+
+    private fun isQualityOverZero(quality: Int) = quality > 0
+
+    private fun decrementQualityBy(item: Item, i: Int) {
+        item.quality -= i
+    }
+
+    private fun isSulfuras(name: String) = name == "Sulfuras, Hand of Ragnaros"
+
+    private fun isBackstagePass(name: String) = name == "Backstage passes to a TAFKAL80ETC concert"
+
+    private fun isAgedBrie(name: String) = name == "Aged Brie"
 
 }
 
